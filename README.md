2da9ef59cc3818098195e6fcf1d40e796b1d500e
JavierMCS <javiermartinezcompsci@gmail.com>
2019-04-01 21:40:08 -0400

    Finished project.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # List Pagination and Filtering
-For this project I am manipulating the DOM in order to hide and show html buttons to I am adding to the page dynamically. This project is currently under process. 
+For this project I am manipulating the DOM in order to hide and show html buttons to I am adding to the page dynamically. This project is finished and I have dynamically added pagination list to my page so I can separate that student list into different pages.
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Project 2
-const page = document.querySelector('.page');
 const studentList = document.querySelector('.student-list');
 let newList = studentList.querySelectorAll('li.student-item');
+
 /*
 This showPage function shows only 10 students per page.
 I have variable newList as a let so it change to number of students in its newList
@@ -17,21 +17,38 @@ const showPage = (list,page) => {
     }
   }
 }
-//showPage(newList, 1);
+showPage(newList, 1);
 //console.log(newList.length); To show that this variable has 54 students in the list
+/*
+This function I am dynamicaly creating pagination links to my page
+so I can separate the student list into different pages.
+*/
 const appendPageLinks = (list) => {
-  let totalList = Math.ceil(list / 10);
+  let totalList = Math.ceil(list.length / 10);
+  const page = document.querySelector('.page');
   const div = document.createElement('div');
+  const ul = document.createElement('ul');
   div.className = 'pagination';
   page.appendChild(div);
-  const ul = document.createElement('ul');
   div.appendChild(ul);
-  for(let i = 1; i < totalList; i++) {
+  //This for loop gives my pages the page numbers at the bottom of the page
+  for(let i = 1; i <= totalList; i++) {
     const li = document.createElement('li');
     const a = document.createElement('a');
     ul.appendChild(li);
     li.appendChild(a);
     a.textContent = i;
+    //Allowing those page numbers at the bottom being able to be clicked.
+    a.addEventListener('click', (e) => {
+      showPage(list, i);
+      const allA = document.querySelectorAll('a');
+      //This highlists the page that I have clicked and that I am on.
+      if(event.target.tagName === 'A')
+        for (let x = 0; x < allA.length; x++){
+          allA[x].classList.remove('active');
+        }
+        e.target.className = 'active';
+    });
   }
 }
 appendPageLinks(newList);
